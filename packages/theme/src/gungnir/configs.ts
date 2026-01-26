e741ac1d4daf6dbadaf1b51ca31f6e31f737f646
evenbecker <torettocustom@gmail.com>
2026-01-26 15:08:22 +0100

    config gh pages

--- packages/theme/src/gungnir/configs.ts
+++ packages/theme/src/gungnir/configs.ts
@@ -5,7 +5,7 @@ import { gungnirThemeExtended } from '../extended-theme/config'
 const isProd = process.env.NODE_ENV === 'production'
 
 export const gungnirThemeConfig = gungnirThemeExtended({
-  repo: 'EvnBecker/blog.EvnBecker.io',
+  repo: 'evenbecker/evenbecker.github.io',
   docsDir: 'blog',
   docsBranch: 'main',
   personalInfo: {
